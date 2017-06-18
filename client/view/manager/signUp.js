337af678cd9e5263ce359a5e2825911f857a0c9b
Jay Mounagurusamy <jmounagu@starbucks.com>
2017-06-17 18:45:23 -0700

    Sign up pager error handling

--- client/view/manager/signUp.js
+++ client/view/manager/signUp.js
@@ -8,10 +8,65 @@ Template.signup.events({
          password: template.find( '[name="password"]' ).value.trim()
          
       };
+      var goodToCreateAccount = true;
+      var alertMeassage = [];
+      if ("@uw.edu" != user.email.slice(-7) && ! isAllowedAccount(user.email)) {
+         $('#email-text-div').addClass("has-error has-feedback");
+         $('#email-error-glyphicon').removeClass("error-mesage-display-none");
+         var email_error_message = $("#email-error-message");
+         email_error_message.removeClass("error-mesage-display-none");
+         email_error_message.addClass("error-mesage-display");
+         alertMeassage.push('At this time only "UW" email id with "@uw.edu" is supported');
+         goodToCreateAccount = false;
+      } else{
+         $("#email-error-message").addClass("error-mesage-display-none");
+         $('#email-text-div').removeClass("has-error has-feedback");
+         $('#email-error-glyphicon').addClass("error-mesage-display-none");
+      }
 
-      if( validationPassword === user.password 
-         && ("@uw.edu" === user.email.slice(-7)|| isAllowedAccount(user.email))) {
-         
+      if( validationPassword != user.password ) {
+         $('#password-one-text-div').addClass("has-error has-feedback");
+         $('#password-two-text-div').addClass("has-error has-feedback");
+         $('#password-one-error-glyphicon').removeClass("error-mesage-display-none");
+         $('#password-two-error-glyphicon').removeClass("error-mesage-display-none");
+         var password_no_match_error_message = $("#password-no-match-error-message");
+         password_no_match_error_message.removeClass("error-mesage-display-none");
+         password_no_match_error_message.addClass("error-mesage-display");
+         goodToCreateAccount = false;
+         alertMeassage.push('Entered password values didnt match, please make sure they match.');
+      } else {
+         $("#password-no-match-error-message").addClass("error-mesage-display-none");
+         $('#password-one-text-div').removeClass("has-error has-feedback");
+         $('#password-two-text-div').removeClass("has-error has-feedback");
+         $('#password-one-error-glyphicon').addClass("error-mesage-display-none");
+         $('#password-two-error-glyphicon').addClass("error-mesage-display-none");
+      }
+
+      console.log($('#terms-condions-agree').is(':checked'));
+      if( ! $('#terms-condions-agree').is(':checked'))
+      {
+         var terms_error_message = $("#terms-error-message");
+         terms_error_message.removeClass("error-mesage-display-none");
+         terms_error_message.addClass("error-mesage-display");
+         goodToCreateAccount = false;
+         alertMeassage.push('Please accept terms and condition by checking the box below.');
+      } else {
+         $("#terms-error-message").addClass("error-mesage-display-none")
+      }
+
+      if (! goodToCreateAccount)
+      {
+         var bertAlertMeassage="";
+         for (var key in alertMeassage)
+         {
+            bertAlertMeassage += alertMeassage[key]
+            bertAlertMeassage += '\n';
+         }
+         Bert.alert(bertAlertMeassage, 'danger')
+      }
+
+
+      if (goodToCreateAccount) {
          Accounts.createUser( user, ( error ) => {
             if ( error ) {
                Bert.alert( error.reason, 'danger' );
@@ -28,11 +83,7 @@ Template.signup.events({
                });
             }
          });
-      }else {
-         document.getElementById("confirm").style.borderColor="red";
-         Bert.alert('Your two password values didnt match or you did not enter a UW email.', 'danger');
-      }
-      
+      }  
    },
 
    'click .terms':function() {
