3cc8ea602729d80cbe3e531b821c880b89a9a8a7
Jay Mounagurusamy <jmounagu@starbucks.com>
2017-06-22 10:20:32 -0700

    allow list, display media

--- client/view/manager/searchResults.js
+++ client/view/manager/searchResults.js
@@ -3,5 +3,6 @@ Template.searchResults.helpers({
    getSearchResults:function() {
       var searchResult = Session.get('SearchResult');
       return searchResult.results;    
-   } 
+   },
+   
 })
