fa0d26f7fb87af3932d438e8a29fcaa39b0cc587
Karthikyokesh Jayakumar <Karthikyokesh Jayakumar>
2017-06-22 18:44:45 -0700

    user

--- client/view/manager/searchResults.js
+++ client/view/manager/searchResults.js
@@ -9,6 +9,11 @@ Template.searchResults.helpers({
    isVerifiedUser: function() {
       var userProfile = UserProfile.findOne({userId: Meteor.userId()});
       return userProfile && Meteor.user().emails[ 0 ].verified;
+   },
+
+   isUser:function() {
+      var userProfile = UserProfile.findOne({userId: Meteor.userId()});
+      return userProfile;
    }
 })
 
